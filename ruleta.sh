74896f684dd6e1a0b07ff84e43827376e33886f5
atentohouston <122824700+atentohouston@users.noreply.github.com>
2026-05-23 18:48:00 -0400

    Add files via upload

---
+++ ruleta.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+
+function ctrl_c(){
+  echo -e "${redColour}\n\n[!] Saliendo...\n${endColour}"
+  tput cnorm; exit 1
+  
+}
+
+# Ctrl+C
+trap ctrl_c INT
+
+function helpPanel(){
+  echo -e "\n${yellowColour}[+]${endColour} ${grayColour}uso:${endColour} ${purpleColour}$0${endColour}\n"
+  echo -e "\t${blueColour}-m)${endColour}${grayColour} Dinero con el que se desea jugar${endColour}"
+  echo -e "\t${blueColour}-m)${endColour}${grayColour} Técnica a utilizar${endColour}${purpleColour} (${endColour}${yellowColour}martingala${endColour}${blueColour}/${endColour}${yellowColour}inverseLabrouchere${endColour}${purpleColour})${endColour}\n"
+
+  exit 1
+}
+function martingala(){
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Dinero actual: ${endColour}${yellowColour}$money$ ${endColour}"
+    echo -ne "${yellowColour}[+]${endColour}${grayColour} ¿Cuánto dinero tienes pensado apostar? -> ${endColour}" && read initial_bet
+    echo -ne "${yellowColour}[+]${endColour}${grayColour} ¿A qué deseas apostar continuamente (par/impar)? ->${endColour} " && read par_impar
+
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Vamos a jugar con una cantidad inicial de ${endColour}${yellowColour}$initial_bet$ ${endColour}${grayColour} a${endColour}${yellowColour} $par_impar${endColour}"
+
+    backup_bet=$initial_bet
+    play_counter=1
+    jugadas_malas=""
+
+    tput civis # Ocultar el cursor
+    while true; do
+        sleep 2.4
+        money=$(($money-$initial_bet))
+        echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Acabas de apostar${endColour}${yellowColour} $initial_bet$ ${endColour}${grayColour} y tienes${endColour}${yellowColour} $money$ ${endColour}"
+        random_number="$(($RANDOM % 37))"
+        echo -e "${yellowColour}[+]${endColour} ${grayColour}Ha salido el número ${endColour}${yellowColour}$random_number${endColour}"
+
+        if [ ! "$money" -lt 0 ]; then
+            if [ "$par_impar" == "par" ]; then
+                # Toda esta definición es para cuando apostamos por números pares
+                if [ "$(($random_number % 2))" -eq 0 ]; then
+                    if [ "$random_number" -eq 0 ]; then
+                        echo -e "${redColour}[+] Ha salido el 0, por tanto perdemos${endColour}"
+                        initial_bet=$(($initial_bet*2))
+                        jugadas_malas+="$random_number "
+                        echo -e "${yellowColour}[+]${endColour}${grayColour} Ahora mismo te quedas en ${endColour}${yellowColour}$money$ ${endColour}"
+                    else
+                        echo -e "${yellowColour}[+]${endColour}${greenColour} El número que ha salido es par, ¡ganas!${endColour}"
+                        reward=$(($initial_bet*2))
+                        money=$(($money+$reward))
+                        echo -e "${yellowColour}[+]${endColour}${grayColour} Ganas un total de ${endColour}${yellowColour}$reward$ ${endColour}"
+                        echo -e "${yellowColour}[+]${endColour}${grayColour} Tienes ${endColour}${yellowColour}$money$ ${endColour}"
+                        initial_bet=$backup_bet
+                        jugadas_malas=""
+                    fi
+                else
+                    echo -e "${yellowColour}[+]${endColour}${redColour} El número que ha salido es impar, ¡pierdes!${endColour}"
+                    initial_bet=$(($initial_bet*2))
+                    jugadas_malas+="$random_number "
+                    echo -e "${yellowColour}[+]${endColour}${grayColour} Ahora mismo te quedas en ${endColour}${yellowColour}$money$ ${endColour}"
+                fi
+            else
+                # Toda esta definición es para cuando apostamos por números impares
+                if [ "$(($random_number % 2))" -eq 1 ]; then
+                    echo -e "${yellowColour}[+]${endColour}${greenColour} El número que ha salido es impar, ¡ganas!${endColour}"
+                    reward=$(($initial_bet*2))
+                    money=$(($money+$reward))
+                    echo -e "${yellowColour}[+]${endColour}${grayColour} Ganas un total de ${endColour}${yellowColour}$reward$ ${endColour}"
+                    echo -e "${yellowColour}[+]${endColour}${grayColour} Tienes ${endColour}${yellowColour}$money$ ${endColour}"
+                    initial_bet=$backup_bet
+                    jugadas_malas=""
+                else
+                    echo -e "${yellowColour}[+]${endColour}${redColour} El número que ha salido es par, ¡pierdes!${endColour}"
+                    initial_bet=$(($initial_bet*2))
+                    jugadas_malas+="$random_number "
+                    echo -e "${yellowColour}[+]${endColour}${grayColour} Ahora mismo te quedas en ${endColour}${yellowColour}$money$ ${endColour}"
+                fi
+            fi
+        else
+            # Nos quedamos sin pasta
+            echo -e "\n${redColour}[!] Te has quedado sin pasta cabrón${endColour}\n"
+            echo -e "${yellowColour}[+]${endColour}${grayColour} Han habido un total de ${endColour}${yellowColour}$(($play_counter-1))${endColour}${grayColour} jugadas${endColour}"
+            echo -e "\n${yellowColour}[+]${endColour}${grayColour} A continuación se van a representar las malas jugadas consecutivas que han salido:${endColour}\n"
+            echo -e "${blueColour}[ $jugadas_malas]${endColour}"
+            tput cnorm; exit 0
+        fi
+
+        let play_counter+=1
+    done
+
+    tput cnorm # Recuperamos el cursor
+}
+
+
+while getopts "m:t:h" arg; do 
+  case $arg in 
+    m) money=$OPTARG;;
+    t) technique=$OPTARG;;
+    h) helpPanel;;
+  esac
+done
+ 
+if [ $money ] && [ $technique ]; then
+  if [ "$technique" == "martingala" ]; then
+    martingala
+  else
+    echo -e "\n${redColour}[!] La técnica introducida no existe${endColour}"
+    helpPanel
+  fi
+else
+  helpPanel  
+fi
