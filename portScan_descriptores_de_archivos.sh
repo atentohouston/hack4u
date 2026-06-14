c539be4ca6b88fc01f606edc48e707e4037e04c9
atentohouston <122824700+atentohouston@users.noreply.github.com>
2026-06-13 22:20:02 -0400

    Implement port scanning functionality in script
    Added functionality to scan ports on a given IP address using TCP connections.

--- portScan_descriptores_de_archivos.sh
+++ portScan_descriptores_de_archivos.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
+
 function ctrl_c(){
   echo -e "\n\n[!] Saliendo...\n"
   tput cnorm && exit 1
 }
+
 # Ctrl + c 
 trap ctrl_c SIGINT
+
 declare -a ports=( $(seq 1 65535) )
+
 function checkPort(){
   
   (exec 3<> /dev/tcp/$1/$2) 2>/dev/null
+
   if [ $? -eq 0 ]; then
     echo "[+] Host $1 - Port $2 (OPEN)"
   fi 
+
   exec 3<&-
   exec 3>&-
 }
+
 tput civis # Ocultar el cursor
+
 if [ $1 ]; then
   for port in ${ports[@]}; do
     checkPort $1 $port &
@@ -23,3 +31,7 @@ if [ $1 ]; then
 else
   echo -e "\n[!] Uso: $0 <ip-address>\n"
 fi 
+
+wait
+
+tput cnorm
